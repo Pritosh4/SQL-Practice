724941c611ce7739ccb03eeb72e2137beda25859
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2023-08-23 16:33:47 +0530

    Update Data Driven Decision Making with advanced SQL queries.sql

--- DataCamp/ Data-Driven Decision Making in SQL/Data Driven Decision Making with advanced SQL queries.sql
+++ DataCamp/ Data-Driven Decision Making in SQL/Data Driven Decision Making with advanced SQL queries.sql
@@ -40,8 +40,44 @@ WHERE movie_id IN
 		(SELECT AVG(rating)
 		 FROM renting));
 
+-- Count movie rentals of customer 45
+SELECT COUNT(*)
+FROM renting AS r
+WHERE r.customer_id = 45;
 
+-- Select customers with less than 5 movie rentals
+SELECT *
+FROM customers as c
+WHERE 5 >
+	(SELECT count(*)
+	FROM renting as r
+	WHERE r.customer_id = c.customer_id);
 
+-- Calculate the minimum rating of customer with ID 7
+SELECT MIN(rating)
+FROM renting
+WHERE customer_id = 7;
+
+SELECT *
+FROM customers AS c
+WHERE 4 > -- Select all customers with a minimum rating smaller than 4 
+	(SELECT MIN(rating)
+	FROM renting AS r
+	WHERE r.customer_id = c.customer_id);
+
+SELECT *
+FROM customers AS c
+WHERE 4 > -- Select all customers with a minimum rating smaller than 4 
+	(SELECT MIN(rating)
+	FROM renting AS r
+	WHERE r.customer_id = c.customer_id);
+
+SELECT *
+FROM movies AS m
+WHERE 8 < -- Select all movies with an average rating higher than 8
+	(SELECT AVG(rating)
+	FROM renting AS r
+	WHERE r.movie_id = m.movie_id);
 
 
 
