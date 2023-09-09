b25b7bf0a545924cea929eef955a29915747df0d
Pritosh Thomas Zachariah <93176385+Pritosh4@users.noreply.github.com>
2023-09-09 23:06:18 +0530

    Update Data Driven Decision Making with OLAP SQL queries.sql

--- DataCamp/ Data-Driven Decision Making in SQL/Data Driven Decision Making with OLAP SQL queries.sql
+++ DataCamp/ Data-Driven Decision Making in SQL/Data Driven Decision Making with OLAP SQL queries.sql
@@ -42,6 +42,50 @@ LEFT JOIN customers AS c
 ON r.customer_id = c.customer_id
 GROUP BY CUBE(country, genre); -- For all aggregation levels of country and genre
 
+SELECT 
+	nationality, -- Select nationality of the actors
+    gender, -- Select gender of the actors
+    COUNT(*) -- Count the number of actors
+FROM actors
+GROUP BY GROUPING SETS ((gender), (nationality), ()); -- Use the correct GROUPING SETS operation
+
+SELECT 
+	country, -- Select country, gender and rating
+    gender,
+    rating
+FROM renting AS r
+LEFT JOIN customers AS c -- Use the correct join
+ON r.customer_id = c.customer_id;
+
+SELECT 
+	c.country, 
+    c.gender,
+	AVG(r.rating) -- Calculate average rating
+FROM renting AS r
+LEFT JOIN customers AS c
+ON r.customer_id = c.customer_id
+GROUP BY c.country, c.gender -- Order and group by country and gender
+ORDER BY c.country, c.gender;
+
+SELECT 
+	c.country, 
+    c.gender,
+	AVG(r.rating)
+FROM renting AS r
+LEFT JOIN customers AS c
+ON r.customer_id = c.customer_id
+GROUP BY GROUPING SETS (c.country), (c.gender); -- Group by country and gender with GROUPING SETS
+
+SELECT 
+	c.country, 
+    c.gender,
+	AVG(r.rating)
+FROM renting AS r
+LEFT JOIN customers AS c
+ON r.customer_id = c.customer_id
+-- Report all info from a Pivot table for country and gender
+GROUP BY GROUPING SETS ((country, gender), (country), (gender), ());
+
 
 
 
